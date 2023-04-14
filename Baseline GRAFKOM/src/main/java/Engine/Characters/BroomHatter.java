7783d919c74aca172399e8c2cd2255e9a305fc29
ChickenNuggetL <119931608+ChickenNuggetL@users.noreply.github.com>
2023-04-14 12:37:37 +0700

    jadiin BroomHatter sama ngetes

--- Baseline GRAFKOM/src/main/java/Engine/Characters/BroomHatter.java
+++ Baseline GRAFKOM/src/main/java/Engine/Characters/BroomHatter.java
@@ -1,4 +1,47 @@
 package Engine.Characters;
 
+import Engine.EasyCreateObject;
+import Engine.Object;
+
+import java.util.ArrayList;
+
 public class BroomHatter {
+    public ArrayList<Object> BroomHatter
+            = new ArrayList<>();
+
+    public void create(){
+        EasyCreateObject Body = new EasyCreateObject();
+        EasyCreateObject LeftFoot = new EasyCreateObject();
+        EasyCreateObject RightFoot = new EasyCreateObject();
+        EasyCreateObject LeftHand = new EasyCreateObject();
+        EasyCreateObject RightHand = new EasyCreateObject();
+        EasyCreateObject hat = new EasyCreateObject();
+
+        Body.Sphere(BroomHatter, 120, 200, 120, 255/255f, 255/255f,0/255f);
+        LeftFoot.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 50, 100, 210/255f, 105/255f,30/255f);
+        RightFoot.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 50, 100, 210/255f, 105/255f,30/255f);
+        LeftHand.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 90, 50, 255/255f, 255/255f,0/255f);
+        RightHand.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 90, 50, 255/255f, 255/255f,0/255f);
+        hat.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(),300, 50f, 300, 0/255f, 0/255f,255/255f);
+
+        BroomHatter.get(0).getChildObject().get(0).translateObject(100/1000f, -180/1000f, 3/100f);
+        BroomHatter.get(0).getChildObject().get(1).translateObject(-100/1000f, -180/1000f, 3/100f);
+        BroomHatter.get(0).getChildObject().get(2).translateObject(100/1000f, -55/1000f, 0f);//tangan kanan
+        BroomHatter.get(0).getChildObject().get(3).translateObject(-100/1000f, -55/1000f, 0f);//tangan kiri
+        BroomHatter.get(0).getChildObject().get(4).translateObject(0f, 100/1000f, 0f);
+
+
+    }
+
+    public void MoveContents(ArrayList<Object> arrayList){
+        for (Object object: BroomHatter){
+            arrayList.add(object);
+        }
+    }
+
+    public void draw(){
+        for (Object object: BroomHatter){
+            object.draw();
+        }
+    }
 }
