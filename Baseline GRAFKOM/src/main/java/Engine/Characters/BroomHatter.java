4dc99d2b2c67fdbaf8de03e63ce037b4ec28afc1
ChickenNuggetL <119931608+ChickenNuggetL@users.noreply.github.com>
2023-04-16 10:31:55 +0700

    broomhatter topi jadi dan gagang broom

--- Baseline GRAFKOM/src/main/java/Engine/Characters/BroomHatter.java
+++ Baseline GRAFKOM/src/main/java/Engine/Characters/BroomHatter.java
@@ -15,7 +15,11 @@ public class BroomHatter {
         EasyCreateObject RightFoot = new EasyCreateObject();
         EasyCreateObject LeftHand = new EasyCreateObject();
         EasyCreateObject RightHand = new EasyCreateObject();
-        EasyCreateObject Hat = new EasyCreateObject();
+        EasyCreateObject Hatbase = new EasyCreateObject();
+        EasyCreateObject hat = new EasyCreateObject();
+        EasyCreateObject hatTail = new EasyCreateObject();
+        EasyCreateObject stick = new EasyCreateObject();
+
 
 
         Body.Sphere(BroomHatter, 120, 200, 120, 255/255f, 255/255f,0/255f);
@@ -23,16 +27,33 @@ public class BroomHatter {
         RightFoot.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 50, 100, 210/255f, 105/255f,30/255f);
         LeftHand.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 90, 50, 200/255f, 200/255f,0/255f);
         RightHand.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(), 50, 90, 50, 200/255f, 200/255f,0/255f);
-        Hat.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(),300, 50f, 300, 0/255f, 0/255f,255/255f);
+        Hatbase.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(),300, 50f, 300, 0/255f, 0/255f,255/255f);
+        hat.EllipticParaboloid((ArrayList<Object>) BroomHatter.get(0).getChildObject(),70, 70, 70, 0/255f, 0/255f,255/255f);
+        hatTail.EllipticParaboloid((ArrayList<Object>) BroomHatter.get(0).getChildObject(),20, 20, 100, 0/255f, 0/255f,255/255f);
+        stick.Sphere((ArrayList<Object>) BroomHatter.get(0).getChildObject(),20, 150f, 20, 181/255f, 101/255f,29/255f);
+
+
 
         BroomHatter.get(0).getChildObject().get(0).translateObject(100/1000f, -180/1000f, -3/100f);
         BroomHatter.get(0).getChildObject().get(1).translateObject(-100/1000f, -180/1000f, -3/100f);
-        BroomHatter.get(0).getChildObject().get(2).rotateObject(1.57f,0.8f,0.7f,-0.25f);//radian(float)derajat,x,y,z)
+
+        BroomHatter.get(0).getChildObject().get(2).rotateObject(1.57f,0.5f,1f,-0f);//radian(float)derajat,x,y,z)
         BroomHatter.get(0).getChildObject().get(2).translateObject(60/1000f, -55/1000f, -10/100f);//tangan kanan
 
-        BroomHatter.get(0).getChildObject().get(3).rotateObject(1.57f,0.8f,0.7f,0.25f);
+        BroomHatter.get(0).getChildObject().get(3).rotateObject(-1.57f,-0.5f,1f,0f);
         BroomHatter.get(0).getChildObject().get(3).translateObject(-60/1000f, -55/1000f, -10/100f);//tangan kiri
+
+
         BroomHatter.get(0).getChildObject().get(4).translateObject(0f, 100/1000f, 0f);
+
+        BroomHatter.get(0).getChildObject().get(5).translateObject(0f, 9/1000f, -1/4f);
+        BroomHatter.get(0).getChildObject().get(5).rotateObject((float)Math.toRadians(90f),1f,0f,0f);
+
+        BroomHatter.get(0).getChildObject().get(6).rotateObject((float)Math.toRadians(180),0f,0.5f,0f);
+        BroomHatter.get(0).getChildObject().get(6).translateObject(1/1000f, 1/4.3f, 1/4f);
+
+        BroomHatter.get(0).getChildObject().get(7).translateObject(0f, -90/1000f, -15/100f);
+
 
     }
 
